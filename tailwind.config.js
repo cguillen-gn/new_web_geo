06417827fad13ec81314147f0d7597081359dac9
cguillen-gn <cguillen@geonet.es>
2026-04-07 08:42:36 +0200

    act

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'geonet-orange': '#ff7700',
+        'geonet-dark': '#1a202c', // Un gris azulado muy oscuro para el texto
+        'geonet-gray': '#f7fafc', // Fondo limpio
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Tipografía moderna y accesible
+      },
+    },
+  },
+}
